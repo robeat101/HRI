e3d0dcb1af5c13cb2ea73abe882cbe2f6462c289
Dan Fitzgerald <danfitz7@hotmail.com>
2014-11-10 15:00:03 -0500

    Can draw lines on overhead view

--- RoboGUI/src/robotGUI/MainWindow.java
+++ RoboGUI/src/robotGUI/MainWindow.java
@@ -9,17 +9,22 @@ import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
+import java.awt.Color;
 
-public class MainWindow {
+//drawing on panels
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+public class MainWindow{
+	
 	private JFrame frame;
 	private JTextField txtSomeTextHere;
 	private JCheckBox chckbxControlSomething;
-	private JPanel panel;
+	private JPanel overheadViewPanel;
 
-	/**
-	 * Launch the application.
-	 */
+	//
+	//Launch the application.
+	//
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -33,19 +38,20 @@ public class MainWindow {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+	//
+	// Create the application.
+	//
 	public MainWindow() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	//
+	// Initialize the contents of the frame.
+	//
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBackground(Color.GRAY);
+		frame.setBounds(100, 100, 1000, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
@@ -63,8 +69,13 @@ public class MainWindow {
 		chckbxControlSomething = new JCheckBox("Control Something");
 		frame.getContentPane().add(chckbxControlSomething, BorderLayout.WEST);
 		
-		panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		overheadViewPanel = new overheadView();
+		overheadViewPanel.setBackground(Color.BLACK);
+		frame.getContentPane().add(overheadViewPanel, BorderLayout.CENTER);
+	}
+	
+	void setupMainWindow(){
+	
 	}
 
 }
