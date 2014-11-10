cf2b6a8aaea1a6484390579019505b73041bc1e9
Dan Fitzgerald <danfitz7@hotmail.com>
2014-11-10 12:49:16 -0500

    Initial GUI Commit

---
+++ RoboGUI/src/robotGUI/MainWindow.java
@@ -0,0 +1,70 @@
+package robotGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+
+public class MainWindow {
+
+	private JFrame frame;
+	private JTextField txtSomeTextHere;
+	private JCheckBox chckbxControlSomething;
+	private JPanel panel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblHello = new JLabel("Hello, welcome to robot world");
+		frame.getContentPane().add(lblHello, BorderLayout.NORTH);
+		
+		txtSomeTextHere = new JTextField();
+		txtSomeTextHere.setText("Some TExt here");
+		frame.getContentPane().add(txtSomeTextHere, BorderLayout.SOUTH);
+		txtSomeTextHere.setColumns(10);
+		
+		JRadioButton rdbtnControlSomething = new JRadioButton("Control something");
+		frame.getContentPane().add(rdbtnControlSomething, BorderLayout.EAST);
+		
+		chckbxControlSomething = new JCheckBox("Control Something");
+		frame.getContentPane().add(chckbxControlSomething, BorderLayout.WEST);
+		
+		panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+	}
+
+}
---
+++ RoboGUI/src/robotGUI/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Dan
+ *
+ */
+package robotGUI;
