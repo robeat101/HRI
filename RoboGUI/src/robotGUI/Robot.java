657d56843f9a49cd4f62e584cad83c420e07bfe5
Dan Fitzgerald <danfitz7@hotmail.com>
2014-11-10 23:47:15 -0500

    Added robot class

---
+++ RoboGUI/src/robotGUI/Robot.java
@@ -0,0 +1,16 @@
+package robotGUI;
+
+import java.awt.Point;
+
+public class Robot {
+	//position, orientation
+	private Point pos;
+	private float theta;
+	
+	//behavior
+	private float intelligence;
+	
+	//goals
+	private Point curGoal;
+	
+}
