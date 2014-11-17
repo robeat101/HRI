94bc087aa530f2ce99aa096a3835a393e85ae375
robeat101 <rbansal@wpi.edu>
2014-11-17 17:54:19 -0500

    Added a standard values class to contain a list of values. Easy to modify global parameters this way.

---
+++ RoboGUI/src/robotGenericValues/StandardValues.java
@@ -0,0 +1,10 @@
+package robotGenericValues;
+
+public class StandardValues {
+
+	public final static float DELTA_THETA = (float) 0.1;
+	public final static int DELTA_TIME = 100; //100 milliseconds
+	public final static int DELTA_POS = 1;
+	
+	
+}
