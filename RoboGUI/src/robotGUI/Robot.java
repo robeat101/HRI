21a8766765d6d55b9622d067e98fd096dd35a7cf
robeat101 <rbansal@wpi.edu>
2014-11-17 17:54:50 -0500

    Added turn method. Added outline for move robot.

--- RoboGUI/src/robotGUI/Robot.java
+++ RoboGUI/src/robotGUI/Robot.java
@@ -7,15 +7,43 @@ package robotGUI;
 
 import java.awt.Point;
 
+import robotGenericValues.StandardValues;
+import robotGenericValues.direction;
+
+
 public class Robot {
-	//position, orientation
-	private Point pos;
-	private float theta;
+	// position, orientation
+	private Point	pos;
+	private float	theta;
+
+	// behavior
+	private float	intelligence;
+
+	// goals
+	private Point	curGoal;
+
+	public void turnRobot(direction turnDirection)
+	{
+		if(turnDirection == direction.LEFT){
+			this.theta = this.theta + StandardValues.DELTA_THETA;
+		}
+		else if (turnDirection == direction.RIGHT) {
+			this.theta = this.theta - StandardValues.DELTA_THETA;
+		}
+	}
 	
-	//behavior
-	private float intelligence;
+	public void moveRobot(direction moveDirection)
+	{
+		if(theta % 0.25 != 0)
+		{
+			//TODO: Add exception
+		}
+		else if(moveDirection == direction.FORWARD){
+			//TODO: Add movement
+		}
+		
+	}
 	
-	//goals
-	private Point curGoal;
+
 	
 }
