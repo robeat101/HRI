f27989915bf87e67fae73fd5dfa96a17fd7875de
Dan Fitzgerald <danfitz7@hotmail.com>
2014-12-16 15:18:42 -0500

    WE CAN HAZ RESULTS!

--- RoboGUI/data/dataParser.pl
+++ RoboGUI/data/dataParser.pl
@@ -4,18 +4,30 @@ use strict;
 use warnings;
 
 
-#Extracted Slicer Parameters
+#Extracted Parameters
+
+my $nRobots=0;
 my @lastBrokenTimes;	#list of times the robots were last broken before being fixed
 my @robotIsBroken;	#keep track of what robots are broken
+my @brokenTimes;
+my @fixedTimes;
+my @cellsToGoWhenFixed;
+my @manaAtFix;
+my $nInstances = 0;
+
+my $outputFilename = "parsed.csv";
 
-#my $outputFilename = $_+'_parsed';
-#open(FH, '<', $filename) or die $!;
 $^I = '.bak';	#save a backup file to appease Windows
 while(<>){	#loop through lines of file	
 	if (/INIT/){
 		if (/Robot world initialized with (\d+) robots/){
-			@lastBrokenTimes = (0) x $1;
-			@robotIsBroken = (0) x $1;		}	}
+			$nRobots = $1;
+			@lastBrokenTimes = (0) x $nRobots;
+			@robotIsBroken = (0) x $nRobots;
+						@brokenTimes = () x $nRobots;
+			@fixedTimes = () x $nRobots;
+			@cellsToGoWhenFixed = () x $nRobots;
+			@manaAtFix = ()x $nRobots;		}	}
 			if (/(\d+): Robot (\d+) broke/){
 		my $robotID = $2;
 		if ($robotIsBroken[$robotID] == 0){
@@ -29,10 +41,26 @@ while(<>){	#loop through lines of file
 		my $cellsToGo = $3;
 		my $mana = $4;
 		
-				print @lastBrokenTimes[$robotID].", ".$time.", ".$robotID.", ".$cellsToGo.", ".$mana."\n";
+		$nInstances+=1;
+		
+		push(@{$brokenTimes[$robotID]}, @lastBrokenTimes[$robotID]);
+		push(@{$fixedTimes[$robotID]}, $time);
+		push(@{$cellsToGoWhenFixed[$robotID]}, $cellsToGo);
+		push(@{$manaAtFix[$robotID]}, $mana);		#print @lastBrokenTimes[$robotID].", ".$time.", ".$robotID.", ".$cellsToGo.", ".$mana."\n";
 
 		@lastBrokenTimes[$robotID] = 0;
 		@robotIsBroken[$robotID] = 0;
 	}
-	#print or die $!;
+	
+
+	print or die $!;
 }
+
+open(my $fh, '>', $outputFilename) or die $!;
+for (my $id=0;$id<$nRobots;$id++){	
+	print $fh "\n\nROBOT ".$id."\nBrokenTimes, FixedTimes, CellsToGo, Mana\n";
+	for (my $i=0;$i<@{$brokenTimes[$id]};$i++){
+		print $fh @{$brokenTimes[$id]}[$i].",".@{$fixedTimes[$id]}[$i].",".@{$cellsToGoWhenFixed[$id]}[$i].",".@{$manaAtFix[$id]}[$i]."\n";
+	}
+}
+close($fh);
